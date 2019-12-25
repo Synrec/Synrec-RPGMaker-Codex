a0c5a93aab598106c061d939d4f84acc196b7637
Synrec <58783714+Synrec@users.noreply.github.com>
2019-12-25 17:26:31 -0400

    Create Kaisyl_Battle_Recovery.js

---
+++ Kaisyl_Battle_Recovery.js
@@ -0,0 +1,63 @@
+/*:
+ *@author Kaisyl/Synrec
+ *@plugindesc (v1)Recover HP after battle.
+ *@help Recover HP after battle by enabling this plugin.
+ *
+ *@param Enable Auto Recovery
+ *@desc Enable or Disable recovery after every battle
+ *@default false
+ *@type Boolean
+ *
+ *@param HP Recovery
+ *@desc Control % of how much HP is recovered.
+ *@default 100
+ *@type number
+ *@max 100
+ *@min 0
+ *
+ *@param MP Recovery
+ *@desc Control % of how much MP is recovered.
+ *@default 100
+ *@type number
+ *@max 100
+ *@min 0
+ *
+ *@param TP Recovery
+ *@desc Control how much TP is recovered.
+ *@default 100
+ *@type number
+ *
+ *@param Clear All States
+ *@desc Remove all states?
+ *@default false
+ *@type boolean
+ */
+ 
+function toNumber(str, def) {
+    return isNaN(str) ? def : +(str || def);
+}
+
+var params = PluginManager.parameters('Kaisyl_Battle_Recovery');
+var synrecRecover = params['Enable Auto Recovery'].toLowerCase();
+var hpRecover = toNumber(params['HP Recovery'], 100);
+var mpRecover = toNumber(params['MP Recovery'], 100);
+var tpRecover = toNumber(params['TP Recovery'], 100);
+var clearStates = params['Clear All States'].toLowerCase();
+
+
+kaisylEndOfWar = BattleManager.endBattle;
+BattleManager.endBattle = function(result) {
+    kaisylEndOfWar.call(this, result);
+    if (synrecRecover == "true") {
+        var members = $gameParty.battleMembers();
+        for (var i = 0; i < members.length; i++) {
+           var member = members[i];
+           member.gainHp(Math.floor(member.mhp * hpRecover/100));
+           member.gainMp(Math.floor(member.mmp * mpRecover/100));
+           member.gainTp(tpRecover);
+           if (clearStates == "true") {
+              member.clearStates();
+           }
+        }
+    }
+};
