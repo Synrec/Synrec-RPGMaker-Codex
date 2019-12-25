b4861b66554ab9aab8a0a629a0970af9c9e5bd4a
Synrec <58783714+Synrec@users.noreply.github.com>
2019-12-25 18:12:00 -0400

    Update Kaisyl_Battle_Recovery.js

--- Kaisyl_Battle_Recovery.js
+++ Kaisyl_Battle_Recovery.js
@@ -2,6 +2,7 @@
  *@author Kaisyl/Synrec
  *@plugindesc (v1)Recover HP after battle.
  *@help Recover HP after battle by enabling this plugin.
+ *Credit given to Silva for assistance in creating this script.
  *
  *@param Enable Auto Recovery
  *@desc Enable or Disable recovery after every battle
@@ -55,11 +56,12 @@ kaisylEndOfWar = BattleManager.endBattle;
 BattleManager.endBattle = function(result) {
     kaisylEndOfWar.call(this, result);
     if (synrecRecover == "true") {
-        var members = $gameParty.battleMembers();
-        for (var i = 0; i < members.length; i++) {
-           var member = members[i];
-           member.gainHp(Math.floor(member.mhp * hpRecover/100));
-           member.gainMp(Math.floor(member.mmp * mpRecover/100));
+        var party = $gameParty.battleMembers();
+        for (var mem = 0; mem < party.length; mem++) {
+           var member = party[mem];
+		   console.log(party);
+           member.gainHp(Math.ceil(member.mhp * hpRecover/100));
+           member.gainMp(Math.ceil(member.mmp * mpRecover/100));
            member.gainTp(tpRecover);
            if (clearStates == "true") {
               member.clearStates();
