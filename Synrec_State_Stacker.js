5bea7c7a107c0202ff5fbfe8e0325b2741a6278b
Synrec <58783714+Synrec@users.noreply.github.com>
2020-02-08 21:24:41 -0400

    Update Synrec_State_Stacker.js

--- Synrec_State_Stacker.js
+++ Synrec_State_Stacker.js
@@ -1,6 +1,6 @@
 /*:
  *@author Kaisyl/Synrec
- *@plugindesc (vTEST) Allows State stacking
+ *@plugindesc (v1) Allows State stacking
  *@help Use the tag <stateStackable:x> to allow a state to stack.
  *Use <stackBurst:x> to remove all state stacks and add a new state
  *when max stack conditions are met.
@@ -9,7 +9,6 @@
  *DO NOT RESELL THIS SCRIPT AS IS.
  *FREE TO USE IN FREE OR COMMERCIAL PROJECTS.
  *DO NOT MODIFY AND THEN ASK FOR MY HELP.
- *CURRENTLY BEING WORKED ON!!! THIS SCRIPT IS EXPERIMENTAL!!
  *
  *@param State Overflow
  *@default false
@@ -19,9 +18,6 @@
  
 var params = PluginManager.parameters('Synrec_State_Stacker');
 var overFlow = params['State Overflow'].toLowerCase();
-console.log(overFlow);
-
-var maxStateStack = 0;
 var stateCount = 0;
 var stateStackable = 0;
 var stackBurst = 0;
@@ -37,24 +33,47 @@ Game_Battler.prototype.addState = function(stateId) {
 	}
 	stateStackable = parseInt($dataStates[stateId].meta.stateStackable);
 	stackBurst = parseInt($dataStates[stateId].meta.stackBurst);
-	if (isNaN(stateStackable) == false  && isNaN(stackBurst) == false){
+	if (isNaN(stateStackable) == false  ||(isNaN(stateStackable) == false && isNaN(stackBurst) == false)){
+		stackBurstActive = true;
 		if (stateStackable !== 0){
-			if (stateStackable !== 0){
-				maxStateStack = stateStackable;
-			}
-			if (stateCount <= maxStateStack){
+			if (stateCount <= stateStackable){
 				this.addNewState(stateId);
 				stateCount++;
 				this.refresh;
 			}
-			if (stateCount >= maxStateStack && stackBurst !== 0){
-				for (i = 0; i < stateCount; i++){
-					this.removeState(stateId);
-					stateCount--;
-					this.refresh;
-					console.log(i + ' ' + stateId);
-					console.log(this);
+			if (stackBurstActive == true && stateCount >= stateStackable){
+				if (this.isStateAddable(stackBurst)) {
+					if (!this.isStateAffected(stackBurst)) {
+						for (i = 0; i < stateCount; i++){
+							this.removeState(stateId);
+							this.refresh;
+						}
+						stateCount = 0;
+						this.addNewState(stackBurst);
+						this.refresh();
+					}
+					if ($dataStates[stackBurst].meta.stateStackable !== 0){
+						stateBurstStack = parseInt($dataStates[stackBurst].meta.stateStackable);
+						stackBurst2 = parseInt($dataStates[stackBurst].meta.stackBurst)
+						stateCount = 0;
+						for (i = 0; i < stateBurstStack; i++){
+							if (this._states[i] == stackBurst){
+							stateCount++;
+							}
+						}
+				if (stateCount >= stateBurstStack){
+					for (i = 0; i < stateCount; i++){
+						this.removeState(stackBurst);
+					}
+					this.addNewState(stackBurst2);
+					this.refresh();
 				}
+			}
+			this.resetStateCounts(stackBurst);
+			this._result.pushAddedState(stackBurst);
+		}
+    }
+			if (stateCount >= stateStackable && stackBurst !== 0){
 				if (overFlow == 'true'){
 					overFlowStateCount = stateCount;
 					for (i = 0; i < overFlowStateCount; i++){
@@ -62,21 +81,12 @@ Game_Battler.prototype.addState = function(stateId) {
 						stateCount--;
 					}
 				}
-				stackBurstActive = true;
+				
 			}
 		}
 	}
-	if (stackBurstActive == true){
-		if (this.isStateAddable(stackBurst)) {
-			if (!this.isStateAffected(stackBurst)) {
-				this.addNewState(stackBurst);
-				this.refresh();
-				console.log('burst');
-			}
-			this.resetStateCounts(stackBurst);
-			this._result.pushAddedState(stackBurst);
-		}
-    }
-	synrecAddState.call(this, stateId);
+	if (stateStackable !== false){
+		synrecAddState.call(this, stateId);
+	}
 	stackBurstActive = false;
 };
