a43514a0b7ee59c7a7e092d2da12be6a7d159465
Synrec <58783714+Synrec@users.noreply.github.com>
2020-02-09 00:28:41 -0400

    Update Synrec_State_Stacker.js
    Finalized.

--- Synrec_State_Stacker.js
+++ Synrec_State_Stacker.js
@@ -1,14 +1,25 @@
 /*:
  *@author Kaisyl/Synrec
- *@plugindesc (v1) Allows State stacking
+ *@plugindesc (v3) Allows State stacking
  *@help Use the tag <stateStackable:x> to allow a state to stack.
  *Use <stackBurst:x> to remove all state stacks and add a new state
  *when max stack conditions are met.
+ *Use <stackOverFlow:x> to indicate an overflow state, the state
+ *that is added after burst conditions are met. Overflow states
+ *are added only once.
  *
  *Where x = state ID; eg: Knockout is generally state ID '1' so x = 1.
  *DO NOT RESELL THIS SCRIPT AS IS.
  *FREE TO USE IN FREE OR COMMERCIAL PROJECTS.
  *DO NOT MODIFY AND THEN ASK FOR MY HELP.
+ *THIS SCRIPT WILL BE OPTIMIZED IN THE FUTURE!
+ *
+ *Changelog:
+ *v1: Released publically.
+ *v2: Fixed errors causing the secondary burst effect to not occur.
+ *Fixed the problem of burst effect not stacking.
+ *v3: Removed secondary burst effect for optimization reasons.
+ *State overflow instead allows for an alternative state post burst.
  *
  *@param State Overflow
  *@default false
@@ -21,72 +32,43 @@ var overFlow = params['State Overflow'].toLowerCase();
 var stateCount = 0;
 var stateStackable = 0;
 var stackBurst = 0;
-var stackBurstActive = false;
 
 synrecAddState = Game_Battler.prototype.addState;
 Game_Battler.prototype.addState = function(stateId) {
 	stateCount = 0;
+	stateStackable = parseInt($dataStates[stateId].meta.stateStackable);
+	stackBurst = parseInt($dataStates[stateId].meta.stackBurst);
 	for (i = 0; i < this._states.length; i++){
 		if (this._states[i] == stateId){
 			stateCount++;
 		}
 	}
-	stateStackable = parseInt($dataStates[stateId].meta.stateStackable);
-	stackBurst = parseInt($dataStates[stateId].meta.stackBurst);
-	if (isNaN(stateStackable) == false  ||(isNaN(stateStackable) == false && isNaN(stackBurst) == false)){
-		stackBurstActive = true;
+	if ((isNaN(stateStackable) == false  ||(isNaN(stateStackable) == false && isNaN(stackBurst) == false)) && !this.isStateAffected(stackBurst)){
 		if (stateStackable !== 0){
-			if (stateCount <= stateStackable){
-				this.addNewState(stateId);
+			if (stateCount <= stateStackable && !this.isStateAffected(stackBurst)){
+				this.addNewState(stateId); //addState.
 				stateCount++;
 				this.refresh;
-			}
-			if (stackBurstActive == true && stateCount >= stateStackable){
-				if (this.isStateAddable(stackBurst)) {
-					if (!this.isStateAffected(stackBurst)) {
-						for (i = 0; i < stateCount; i++){
-							this.removeState(stateId);
-							this.refresh;
-						}
-						stateCount = 0;
-						this.addNewState(stackBurst);
-						this.refresh();
-					}
-					if ($dataStates[stackBurst].meta.stateStackable !== 0){
-						stateBurstStack = parseInt($dataStates[stackBurst].meta.stateStackable);
-						stackBurst2 = parseInt($dataStates[stackBurst].meta.stackBurst)
-						stateCount = 0;
-						for (i = 0; i < stateBurstStack; i++){
-							if (this._states[i] == stackBurst){
-							stateCount++;
-							}
-						}
-				if (stateCount >= stateBurstStack){
-					for (i = 0; i < stateCount; i++){
-						this.removeState(stackBurst);
-					}
-					this.addNewState(stackBurst2);
-					this.refresh();
-				}
-			}
-			this.resetStateCounts(stackBurst);
-			this._result.pushAddedState(stackBurst);
+			}	
 		}
-    }
-			if (stateCount >= stateStackable && stackBurst !== 0){
-				if (overFlow == 'true'){
-					overFlowStateCount = stateCount;
-					for (i = 0; i < overFlowStateCount; i++){
-						this.addNewState(stateId);
-						stateCount--;
-					}
-				}
-				
+		if (stateCount >= stateStackable && !this.isStateAffected(stackBurst) && stackBurst != 0){
+			for (i = 0; i < stateCount; i++){
+				this.removeState(stateId);
+			}
+			this.addNewState(stackBurst);
+			this.refresh();
+		}
+		if (overFlow == 'true' && this.isStateAffected(stackBurst)){
+			var overFlowState = parseInt($dataStates[stateId].meta.stackOverFlow);
+			console.log('almost...');
+			if (isNaN(overFlowState) == false){
+				this.addNewState(overFlowState);
+				this.refresh;
+				console.log('victor');
 			}
 		}
 	}
-	if (stateStackable !== false){
+	if (isNaN(stateStackable) == true || isNaN(stackBurst) == true){
 		synrecAddState.call(this, stateId);
 	}
-	stackBurstActive = false;
 };
